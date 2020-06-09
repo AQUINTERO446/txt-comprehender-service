946d928e678682f291f992249880b36577450e3c
Andres Quintero Galvan <aquintero446@gmail.com>
2020-06-09 10:59:53 -0500

    test no comments

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker run -dit --rm --name test_comprehend -p 9999:80 $DOCKER_REPO
 sleep 5
-# curl -X POST -F 'tpk_file=@/tests/alt_root_name.tpk' 'http://localhost:9999/convert'
 curl -X GET 'http://localhost:9999/health'
 docker stop test_comprehend
